cdcda61ef34fe53d0cf6aaf20f1cd54029dda767
CMyers79 <codermyers@gmail.com>
2023-12-04 19:53:10 -0800

    update template

--- template.py
+++ template.py
@@ -4,4 +4,12 @@ from collections import deque
 with open('input.txt') as file:
     strings = file.readlines()
 s_list = [string.strip() for string in strings]
+out = 0
+out2 = 0
 for i, s in enumerate(s_list):
+
+
+
+
+print(out)
+print(out2)
